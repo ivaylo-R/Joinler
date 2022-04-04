26f0fc3c6ba4629b4109727d9a5c0aa2e500e2ba
Ivaylo Rusinov <ivaylor2@gmail.com>
2022-04-04 13:48:03 +0300

    Create DB

--- src/Data/ApplicationDbContext.cs
+++ src/Data/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Joinler.Data.Configurations;
+using Joinler.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Joinler.Data
@@ -8,6 +10,27 @@ namespace Joinler.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+
+        public DbSet<Comment> Comments { get; set; }
+
+        public DbSet<Friendship> Friendships { get; set; }
+
+        public DbSet<Group> Groups { get; set; }
+
+        public DbSet<Like> Likes { get; set; }
+
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new FriendshipConfiguration());
+            builder.ApplyConfiguration(new CommentConfiguration());
+            builder.ApplyConfiguration(new LikeConfiguration());
+
+            base.OnModelCreating(builder);
         }
     }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -24,9 +24,9 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -40,5 +40,11 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
